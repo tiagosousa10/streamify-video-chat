54f431894aa9866a9384a6c2fa00f21f8c09041d
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-12 11:26:28 +0100

    testing auth controllers

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export async function signup(req,res) {
+  res.send("signup")
+}
+
+export async function login(req,res) {
+  res.send("login")
+}
+
+export async function logout(req,res) {
+  res.send("logout")
+}
+
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,19 +1,11 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
-router.get("/signup", (req,res) => {
-  res.send("signup")
-})
-
-router.get("/login", (req,res) => {
-  res.send("login")
-})
-
-
-router.get("/logout", (req,res) => {
-  res.send("logout")
-})
+router.get("/signup", signup)
+router.get("/login", login)
+router.get("/logout", logout)
 
 
 export default router;
