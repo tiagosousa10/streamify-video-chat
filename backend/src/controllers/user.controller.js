8a4d76666bfd1014e556404cb82ffa6567dc51ed
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-14 16:06:02 +0100

    get friend request and out going friends request

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -119,3 +119,39 @@ export async function acceptFriendRequest(req,res) {
     res.status(500).json({message: "Internal server error"})
   }
 }
+
+export async function getFriendRequests(req,res) {
+  try {
+
+    const incomingReqs = await FriendRequest.find({
+      recipient: req.user.id,
+      status: "pending",
+    }).populate("sender", "fullName profilePic nativeLanguage learningLanguage")
+
+
+    const acceptedReqs = await FriendRequest.find({
+      sender: req.user.id,
+      status: "accepted",
+    }).populate("recipient", "fullName profilePic")
+
+    res.status(200).json({incomingReqs, acceptedReqs})
+
+  } catch(error) {
+    console.log("Error in getFriendRequests controller",error);
+    res.status(500).json({message: "Internal server error"})
+  }
+}
+
+export async function getOutgoingFriendReqs(req,res) {
+  try {
+    const outgoingRequests = await FriendRequest.find({
+      sender: req.user.id,
+      status: "pending",
+    }).populate("recipient", "fullName profilePic nativeLanguage learningLanguage")
+
+    res.status(200).json(outgoingRequests)
+  } catch(error) {
+    console.log("Error in getOutgoingFriendReqs controller",error);
+    res.status(500).json({message: "Internal server error"})
+  }
+}
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js"
-import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
+import { acceptFriendRequest, getFriendRequests, getMyFriends, getOutgoingFriendReqs, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -13,5 +13,7 @@ router.get("/friends", getMyFriends)
 
 router.post("/friend-request/:id", sendFriendRequest)
 router.put("/friend-request/:id/accept", acceptFriendRequest)
+router.get("/friend-requests", getFriendRequests)
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs)
 
 export default router;
