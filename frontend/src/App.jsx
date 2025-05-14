0644069312a27c261616b371124fea41d1911a11
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-14 18:42:58 +0100

    first routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import NotificationPage from './pages/NotificationPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div className=''><button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button></div>
+    <div className='h-screen' data-theme="night">
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/call' element={<CallPage />} />
+        <Route path='/chat' element={<ChatPage />} />
+        <Route path='/onboarding' element={<OnboardingPage />} />
+      </Routes>
+
+      <Toaster />
+    </div>
   )
 }
 
