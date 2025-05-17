792d95aa622b240389f3c1d9a2013466df4993fb
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-17 11:11:49 +0100

    onboarding page mutation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,18 +15,25 @@ const App = () => {
   //create, delete , ect -> use mutation
   const {isLoading, authUser} = useAuthUser()
 
+  const isAuthenticated = Boolean(authUser)
+  const isOnboarded = authUser?.isOnboarded;
+
   if(isLoading) return <PageLoader />
 
   return (
     <div className='h-screen' data-theme="night">
       <Routes>
-        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path='/signup' element={!authUser ?<SignUpPage /> : <Navigate to="/" />} />
-        <Route path='/login' element={!authUser ?  <LoginPage /> : <Navigate to="/" />} />
-        <Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/login" />} />
-        <Route path='/call' element={authUser ? <CallPage /> : <Navigate to="/login" />} />
-        <Route path='/chat' element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
-        <Route path='/onboarding' element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
+        <Route path='/' element={isAuthenticated && isOnboarded ? (
+          <HomePage/>
+        ) : (
+          <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+        )} />
+        <Route path='/signup' element={!isAuthenticated ?<SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/login' element={!isAuthenticated ?  <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/notifications' element={isAuthenticated ? <NotificationPage /> : <Navigate to="/login" />} />
+        <Route path='/call' element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path='/chat' element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path='/onboarding' element={isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />} />
       </Routes>
 
       <Toaster />
--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useAuthUser from '../hooks/useAuthUser'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+import { completeOnboarding } from '../lib/api'
 
 const OnboardingPage = () => {
-  const {isLoading, authUser} = useAuthUser()
+
+  const {authUser} = useAuthUser()
+  const queryClient = useQueryClient()
+
+  const [formState, setFormState] = useState({
+    fullName: authUser.fullName || "",
+    bio: authUser.bio || "",
+    nativeLanguage: authUser.nativeLanguage || "",
+    learningLanguage: authUser.learningLanguage || "",
+    location: authUser.location || "",
+    profilePic: authUser.profilePic || "",
+  })
+
+  const {mutate: onboardingMutation, isPending} =useMutation({
+    mutationFn: completeOnboarding, // api call to complete onboarding -> POST
+    onSuccess: () => {
+      toast.success("Profile onboarded successfully")
+      queryClient.invalidateQueries({queryKey: ["authUser"]}) // to refetch
+    }
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onboardingMutation(formState)
+  }
 
   return (
-    <div>OnboardingPage</div>
+    <div className=''>
+
+    </div>
   )
 }
 
