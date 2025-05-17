408b826f3688487ec7c368821baf9463610be1de
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-17 09:50:47 +0100

    right side of sign up page jsx added

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -18,7 +18,8 @@ const SignUpPage = () => {
   return (
     <div className='h-screen flex items-center justify-center p-4 sm:p-6 md:p-8' data-theme="forest">
       <div className='border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden'>
-        {/*signup form - LEFT SIDE */}
+
+        {/*SIGNUP FORM - LEFT SIDE */}
         <div className='w-full lg:w-1/2 p-4 sm:p-8 flex flex-col'>
 
         {/*LOGO */}
@@ -117,6 +118,24 @@ const SignUpPage = () => {
 
         </div>
 
+        </div>
+
+        {/*SIGNUP FORM - RIGHT SIDE */}
+        <div className='hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center'>
+          <div className='max-w-md p-8'>
+            {/* ilustration */}
+            <div className='relative aspect-square max-w-sm mx-auto'>
+              <img src="/i.png" alt="language learning" className='w-full h-full' />
+            </div>
+
+            <div className='text-center space-y-3 mt-6'>
+              <h2 className='text-xl font-semibold'>Connect with language partners worldwide</h2>
+              <p className='opacity-70'>
+                Practice conversations, make friends, and improve your language skills with our global community.
+              </p>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
