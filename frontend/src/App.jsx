fd26373d2b23f82d97018cc3497018aea15a3b89
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-20 15:03:57 +0100

    sidebar updated with navigation links and user profile section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
   if(isLoading) return <PageLoader />
 
   return (
-    <div className='h-screen' data-theme="night">
+    <div className='h-screen' data-theme="forest">
       <Routes>
         <Route path='/' element={isAuthenticated && isOnboarded ? (
           <Layout showSidebar={true}>
