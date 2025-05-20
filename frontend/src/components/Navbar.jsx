eb525c5d6e8c14ddb453ce3aa5d39f1c86a267ba
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-20 15:38:50 +0100

    notifications button added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import useAuthUser from '../hooks/useAuthUser'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { logout } from '../lib/api'
+import { BellIcon, ShipWheelIcon } from 'lucide-react'
+
+
 const Navbar = () => {
 
    const {authUser} = useAuthUser()
@@ -19,8 +22,33 @@ const Navbar = () => {
    })
 
   return (
-    <nav>
-      
+    <nav className='bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center'>
+      <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-end w-full'>
+          
+          {/* LOGO - ONLY IN CHAT PAGE */}
+          {isChatPage && (
+            <div className='pl-5'>
+              <Link to={"/"} className='flex items-center gap-2.5'>
+                <ShipWheelIcon className='size-9 text-primary' />
+                <span className='text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider'>
+                  Streamify
+                </span>
+            </Link>
+            </div>
+          )}
+
+          <div className=' flex items-center gap-3 sm:gap-4 '>
+            <Link to="/notifications">
+              <button className='btn btn-ghost btn-circle'>
+                <BellIcon className='h-6 w-6 text-base-content opacity-70' />
+              </button>
+            </Link>
+
+          </div>
+
+          </div>
+      </div>
     </nav>
   )
 }
