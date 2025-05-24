f1f1a0909e9a0a78219ac7e1a92e7c97dfb2a79b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-24 11:12:05 +0100

    fetch the data for the call page, install video-react-sdk

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -57,7 +57,13 @@ const App = () => {
           <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
         )} />
         
-        <Route path='/call' element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path='/call:id' element={
+          isAuthenticated && isOnboarded ? (
+                <CallPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+        } />
         <Route path='/chat/:id' element={
             isAuthenticated && isOnboarded ? (
               <Layout showSidebar={true}>
--- frontend/src/pages/CallPage.jsx
+++ frontend/src/pages/CallPage.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import useAuthUser from '../hooks/useAuthUser'
+import { useQuery } from '@tanstack/react-query'
+import { getStreamToken } from '../lib/api'
+
+import {
+
+} from "@stream-io/video-react-sdk"
+import "@stream-io/video-react-sdk/dist/css/styles.css"
 
 const CallPage = () => {
+  const {id: callId} = useParams()
+
+  const [client,setClient] = useState(null)
+  const [call, setCall] = useState(null)
+  const [isConnecting,setIsConnecting] = useState(true)
+
+  const {authUser, isLoading} = useAuthUser()
+  
+  const {data: tokenData} = useQuery({
+    queryKey: ["streamToken"],
+    queryFn: getStreamToken,
+    enabled: !!authUser // only fetch if authUser is available
+  })
+
+  useEffect(() => {
+    const initCall = async () => {
+      if(!tokenData || !authUser || !callId) return;
+
+      try {
+        console.log("Initializing call client...")
+
+      } catch(error) {
+        console.log("Error in initCall",error)
+      }
+    }
+
+    initCall()
+  }, [])
+
+
   return (
-    <div>CallPage</div>
+    <div>
+
+    </div>
   )
 }
 
